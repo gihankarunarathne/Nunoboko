a53ab525c9eb4742266623ff826d90e9e1595cdc
Gihan Karunarathne <gckarunarathne@gmail.com>
2014-11-13 14:26:25 +0530

    Added personal files in prelude

---
+++ prelude/personal/preload/editor.el
@@ -0,0 +1,15 @@
+(global-hl-line-mode -1)
+(global-linum-mode 1)
+
+(setq c-basic-offset 2) ; indents 4 chars                                                                                                              
+(setq tab-width 2)          ; and 4 char wide for TAB
+(setq indent-tabs-mode nil) ; And force use of spaces
+;(turn-on-font-lock)       ; same as syntax on in Vim
+;(setq width (max width (+ (length str) 1)))   ; ??? line numbers
+;(setq width (max width (length str)))
+;(setq linum-format "%4d \u2502 ")
+;(setq linum-format "%d")
+
+;(setq linum-highlight-in-all-buffersp t)
+;(hlinum-activate)
+;(setq linum-highlight-face "white")
---
+++ prelude/personal/preload/theme.el
@@ -0,0 +1,13 @@
+;(disable-theme 'zenburn)
+
+(setq prelude-theme 'deeper-blue)
+;(setq prelude-theme 'tangotango)
+;(setq prelude-theme 'tango-2)
+;(setq prelude-theme 'tango-plus)
+;(setq prelude-theme 'naquadah)
+;(setq prelude-theme 'zonokai)
+;(setq prelude-theme 'solarized-dark)
+
+; Add custome theme
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/prelude/themes/emacs-color-theme-darkula")
+;(load-theme 'Darkula t)
